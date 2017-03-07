84064c1d7095fbdd317c623d2c637311bcb219d1
Vernon Louie <vernonlouie@aol.com>
2017-03-07 14:29:03 -0800

    Added 'lift cards' function.  Added comments to .js file.  Deleted most console.logs and commented out sections. -VL

--- js_for_mem_match.js
+++ js_for_mem_match.js
@@ -1,4 +1,4 @@
-/*  Memory Match project v1.0    Vernon Louie    C11.16      11/18/2016     */
+/*  Vernon Louie     March 2017     */
 
 var first_card_clicked = null;
 var second_card_clicked = null;
@@ -10,23 +10,29 @@ var accuracy = 0;
 var games_played = 0;
 
 $(document).ready(function () {
+    insertFrontCards();
+    $(".lift").click(lift_clicked);
+    $(".back").click(card_clicked);     // Call function card_clicked when clicking on a card
+    $(".reset").click(reset_clicked);   // Call function reset_clicked when clicking on the reset button
+});
+
+/* Appends the 9 card fronts (2x) randomly into the 18 slots. */
+function insertFrontCards () {
     var card;
     var card_img;
     var slot;
 
     randomizedArray = generateRandomCardSlots();
-    // console.log("randomizedArray: " + randomizedArray);
 
     for (var h=0; h <= 9; h+=9) {
-        for (var i=1; i <= 9; i++) {
+        for (var i=1; i <= 9; i++) {            // go thru this loop 2x, when h=0 and h=9.
             if (i === 4) {
-                card = "images/pony4b.png";
+                card = "images/pony4b.png";     // pony4 is a png; all the rest are jpg
             } else {
                 card = "images/pony" + i + "b.jpg";
             }
 
             slot = "#slot" + randomizedArray[i-1+h];
-            // console.log("i: " + i + ".  card: " + card + ".  slot: " + slot);
 
             card_img = $("<img>",
                 {
@@ -36,17 +42,14 @@ $(document).ready(function () {
                 });
 
             $(slot).append(card_img);
-        } // end of inner for loop
-    } // end of outer for loop
+        }
+    }
+}
 
-    $(".back").click(card_clicked);     // Call function card_clicked when clicking on a card
-    $(".reset").click(reset_clicked);   // Call function reset_clicked when clicking on the reset button
-});
-
+/* Takes an array (18 elements long) and returns an array of the same elements in random order. */
 function generateRandomCardSlots () {
     var array_ordered = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
     var array_randomized = [];
-    var count = 0;
     var rndm_num;
 
     while (array_ordered.length > 0) {
@@ -56,14 +59,13 @@ function generateRandomCardSlots () {
             if (rndm_num === array_ordered[j]) {
                 array_ordered.splice(j,1);
                 array_randomized.push(rndm_num);
-                // console.log("array_randomized: " + array_randomized + ".  array_ordered: " + array_ordered);
             }
         }
-        // console.log("count: " + count++);
     }
     return array_randomized;
 }
 
+/* Assuming there are 2 cards clicked and therefore opaque (card front is visible), makes the card back visible so card front is no longer visible to user. */
 function resetTwoCards () {
     $(first_card_clicked).toggleClass("make_opaque");
     $(second_card_clicked).toggleClass("make_opaque");
@@ -71,6 +73,7 @@ function resetTwoCards () {
     second_card_clicked = null;
 }
 
+/* Generates or affects text in "stats" area (left_side div). */
 function display_stats () {
     $(".games_played .value").text(games_played);
     $(".matches .value").text(match_counter);
@@ -81,6 +84,7 @@ function display_stats () {
     $(".accuracy .value").text(percent_accuracy + "%");
 }
 
+/* Resets to zero and then displays. */
 function reset_stats () {
     accuracy = 0;
     match_counter = 0;
@@ -88,6 +92,13 @@ function reset_stats () {
     display_stats();
 }
 
+/* "Lifts" card backs for 1/2 second so users can see card fronts momentarily. */
+function lift_clicked () {
+    $(".back").addClass("make_opaque");
+    setTimeout(function() {$(".back").removeClass("make_opaque")}, 500);
+}
+
+/* */
 function reset_clicked () {
     // if (match_counter === total_possible_matches) {
         games_played++;  // only if player has completed game then player gets credit for a game played
@@ -97,9 +108,12 @@ function reset_clicked () {
     reset_stats();
     $(".back").removeClass("make_opaque"); // card backs are put back in place by making them visible again
     $('#game_area h3').remove(); // removes h3 element containing "You have won! Word to the mother!"
+    $(".front").remove();   // remove the card front elements
+    insertFrontCards();
     $(".reset").click(reset_clicked);   // Call function reset_clicked when clicking on the reset button
 }
 
+/* If 1st card clicked, then simply shows card front.  If 2nd card clicked, then checks to see if there is a match with the 1st card. */
 function card_clicked () {
     $(this).toggleClass("make_opaque");
 
@@ -115,7 +129,6 @@ function card_clicked () {
 
         var first_img = $(first_card_clicked).parent().children(".front").find("img").attr('src');
         var second_img = $(second_card_clicked).parent().children(".front").find("img").attr('src');
-        // console.log("attempts: ", attempts);
 
         if (first_img == second_img) {
             match_counter++;
@@ -124,20 +137,13 @@ function card_clicked () {
 
             first_card_clicked = null;
             second_card_clicked = null;
-            // console.log("match counter is: ", match_counter);
 
             if (match_counter === total_possible_matches) {
                 $('#game_area').append("<h3>You have won!  Word to the mother!</h3>");
             }
-            else {
-                return;
-            } // end of 3rd if block
         }
         else {
             setTimeout(resetTwoCards, 1500);
-            return;
-        } // end of 2nd if block
-
-    } // end of 1st if block
-
+        }
+    }
 } // end of function card_clicked
